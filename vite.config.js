cf5e701201a1caf07fb4ef59dc6c52c03fac1298
tetedgaming1 <teted.alt@gmail.com>
2026-03-20 12:34:43 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  logLevel: 'error', // Suppress warnings, only show errors
+  plugins: [
+    react(),
+  ],
+});
